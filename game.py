36d4a8560b27827c55de9e02777f45aaa018c34a
20200700 <znffjr8520@daumnet>
2021-03-30 16:41:40 +0900

    game

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 print("게임을 시작합니다")
 print("테트리스 시작")
-print("1.오른쪽 2.왼쪽 3.블록 바꾸기")
-number = int(input("숫자를 입력하세요: "))
-print("당신이 입력한 숫자는",number)
-if number == 1:
-    print("오른쪽으로 이동")
-if number == 2:
-    print("왼쪽으로 이동")
-if number == 3:
-    print("블록 바꾸기")
- 
+######여기서 부터 반복#######
+for i in range(10):
+    print("1.오른쪽 2.왼쪽 3.블록 바꾸기")
+    number = int(input("숫자를 입력하세요: "))
+    print("당신이 입력한 숫자는",number)
+    if number == 1:
+        print("오른쪽으로 이동")
+    if number == 2:
+        print("왼쪽으로 이동")
+    if number == 3:
+        print("블록 바꾸기")
+#####반복 종료#######
---
+++ test.py
@@ -0,0 +1,8 @@
+#for i in range(2):
+#    print(i)
+#    print("hello world")
+#테트리스 다양한 블록 list로 묶어서 변수 만들기
+block=["ㄹ","ㄱ","ㄴ","ㅁ","ㅜ","ㅣ","ㅂ"]
+print("블록의 갯수:", len(block))
+for i in range(len(block)):
+    print(block[i])
